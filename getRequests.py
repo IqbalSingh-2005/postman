d22caa6c979aedd968921ad25291d9546bbb94c0
Blackstate9080 <is7636665@gmail.com>
2025-09-05 08:09:57 +0530

    Exception handling - Uncompleted

--- getRequests.py
+++ getRequests.py
@@ -15,17 +15,53 @@ class Request:
         print("Body".center(50,"="))
         print(response.text)
 
+#------------------------Exception---------------------------------------
+
+    def _exception_(self, e):
+        if isinstance(e, requests.RequestException):
+            print("An ambiguous exception occurred while handling your request.")
+
+        elif isinstance(e, requests.ConnectionError):
+            print("A Connection error occurred.")
+
+        elif isinstance(e, requests.HTTPError):
+            print("An HTTP error occurred.")
+
+        elif isinstance(e, requests.TooManyRedirects):
+            print("Too many redirects.")
+
+        elif isinstance(e, requests.ConnectTimeout):
+            print("The request timed out while trying to connect to the remote server.\nTry Again")
+
+        elif isinstance(e, requests.ReadTimeout):
+            print("The server did not send any data in the allotted amount of time.")
+
+        elif isinstance(e, requests.Timeout):
+            print("The request timed out.")
+
+        elif isinstance(e, requests.JSONDecodeError):
+            print("Couldn’t decode the text into json")
+
+        else:
+            print("Unknown Error Ocuured\nSEE LOGS")
+        
 #------------------------GET---------------------------------------
 
-    def get(self, params=None):
-        response = requests.get(self.url, params=params)
-        self._print_(response, "GET")
-
+    def get(self, params=None, timeout=10):
+        try:
+            response = requests.get(self.url, params=params)
+            self._print_(response, "GET")
+        except requests.RequestException:
+            self._exception_()
+        
 #------------------------POST---------------------------------------
 
     def post(self, data=None, json=None):
-        response = requests.post(self.url, data=data, json=json)
-        self._print_(response, "POST")
+        try:
+            response = requests.post(self.url, data=data, json=json)
+            self._print_(response, "POST")
+        except requests.RequestException:
+            self._exception_()
 
 #------------------------HEAD---------------------------------------
 
